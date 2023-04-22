70b8159bbfa7172201eb1f07ef62b54c5453522c
olena <denaolena@gmail.com>
2023-04-22 10:30:41 +0300

    зміна в файлі fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
-const BASE_URL = `https://restcountries.com/v3.1/name`;
-const filter = `fields=name,capital,population,flags,languages`;
-
 export function fetchCountries(name) {
+  const BASE_URL = `https://restcountries.com/v3.1/name`;
+  const filter = `fields=name,capital,population,flags,languages`;
+
   return fetch(`${BASE_URL}/${name}?${filter}`).then(response => {
     if (!response.ok) {
       throw new Error(responce.statusText);
